71a593b4014386d4129580bac09cd2e9ea83f1d2
Shishir <shishir3324kumar@gmail.com>
2024-04-25 23:53:52 +0530

    tailwind installed in the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,13 @@ function App() {
 
   return (
     <>
-      <h1>All about Hackathon project</h1>
+      <div className="pt-16">
+
+        <div className="container mx-auto bg-gray-300/25 px-10 py-5" style={{minHeight: "540px"}}>
+          <h2 className="text-3xl font-bold">Hackathon Display Project</h2>
+        </div>
+
+      </div>
     </>
   )
 }

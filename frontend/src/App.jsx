74d9bb157e217112a627299e22cb45f975f45508
Shishir <shishir3324kumar@gmail.com>
2024-04-25 23:48:43 +0530

    frontend initialized with react and vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>All about Hackathon project</h1>
+    </>
+  )
+}
+
+export default App

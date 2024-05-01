d07d26c13a5d8d1610621e19e594b483a1ccf8b6
hereisSwapnil <swapnilskumars@gmail.com>
2024-05-02 00:03:26 +0530

    made hackthon model

--- scraper/main.py
+++ scraper/main.py
@@ -2,20 +2,30 @@ import schedule
 import time
 
 from sites.devpost.devpost import scrapeDevpost
-from utils.saveToDatabase import addHackathonsToMongoDB
+from utils.saveToDatabase import Hackathon
 from sites.devfolio.devfolio import scrapeDevfolio
 
 def scrape_and_save():
-    devfolioHackthons = scrapeDevfolio()
-    devpostHackthons = scrapeDevpost()
-    hackathons = devfolioHackthons + devpostHackthons
-    addHackathonsToMongoDB(hackathons)
+    devfolio_hackathons = scrapeDevfolio()
+    
+    devpost_hackathons = scrapeDevpost()
+    
+    all_hackathons = devfolio_hackathons + devpost_hackathons
+    
+    for hackathon_data in all_hackathons:
+        hackathon = Hackathon(**hackathon_data)
+        hackathon.save_to_db()
+
 
 def job():
-    print("Scraping and saving data")
-    scrape_and_save()
-    print("Data saved successfully")
-
+   try:
+        print("Scraping and saving data")
+        scrape_and_save()
+        print("Data saved successfully")
+   except:
+        print("Error in scraping and saving data")
+        job()
+    
 schedule.every().hour.do(job)
 
 while True:
--- scraper/utils/saveToDatabase.py
+++ scraper/utils/saveToDatabase.py
@@ -1,11 +1,39 @@
+from datetime import datetime
 from db.dbConnect import client
 
-def addHackathonsToMongoDB(hackathon_data):
-    db_name = "hackathons"
-    col_name = "hackathon_data"
-    db = client[db_name]
-    col = db[col_name]
-    col.delete_many({})
-    for hackathon in hackathon_data:
-        col.insert_one(hackathon)
-            
+class Hackathon:
+    def __init__(self, link, name, date, image, location, prizes, participants, themes, status, featured, source):
+        self.link = link
+        self.name = name
+        self.date = date
+        self.image = image
+        self.location = location
+        self.prizes = prizes
+        self.participants = participants
+        self.themes = themes
+        self.status = status
+        self.featured = featured
+        self.source = source
+
+    def save_to_db(self):
+        db_name = "hackathons"
+        col_name = "hackathon_data"
+        db = client[db_name]
+        col = db[col_name]
+        col.insert_one(self.to_dict())
+
+    def to_dict(self):
+        return {
+            "link": self.link,
+            "name": self.name,
+            "date": self.date,
+            "image": self.image,
+            "location": self.location,
+            "prizes": self.prizes,
+            "participants": self.participants,
+            "themes": self.themes,
+            "status": self.status,
+            "featured": self.featured,
+            "source": self.source,
+            "created_at": datetime.now()
+        }

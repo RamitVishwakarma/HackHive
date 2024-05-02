e15e5866ca35c3c647550a34eacc540fb3bb881c
Ojas Mishra <ojasmishra555@gmail.com>
2024-05-02 23:55:34 +0530

    changes in frontend

---
+++ frontend/src/Organizations/Organizations.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Organizations() {
+  return (
+    <div className="organizations">
+        <p></p>
+        <div className="orgs">
+            <a href=""><img src="" alt="" /></a>
+            <a href=""><img src="" alt="" /></a>
+            <a href=""><img src="" alt="" /></a>
+            <a href=""><img src="" alt="" /></a>
+            <a href=""><img src="" alt="" /></a>
+            <a href=""><img src="" alt="" /></a>
+            <a href=""><img src="" alt="" /></a>
+        </div>
+    </div>
+  )
+}
---
+++ frontend/src/Serarch Bar/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Search() {
+  return (
+    <div className="search">
+        <input type="text" id='search-bar' placeholder='Search for Hackathons' />
+        <button>Search</button>
+    </div>
+  )
+}
